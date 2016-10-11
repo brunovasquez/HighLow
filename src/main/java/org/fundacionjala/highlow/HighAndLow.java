455a42c57253159280031071dbb23cb851125f07
bruno vasquez <bruno.vasquez@fundacion-jala.org>
2016-10-11 13:40:56 -0400

    Ready To Code Review HighLow

---
+++ src/main/java/org/fundacionjala/highlow/HighAndLow.java
@@ -0,0 +1,29 @@
+package org.fundacionjala.highlow;
+
+import java.util.Arrays;
+
+/**
+ * Class that searc high and low number.
+ */
+final class HighAndLow {
+
+    /**
+     * Private constructor.
+     */
+    private HighAndLow() {
+
+    }
+
+    /**
+     * Method to add get the high and low number.
+     *
+     * @param numbers a String that contain the numbers.
+     * @return the high and low numbers as a string.
+     */
+    static String getHighLow(final String numbers) {
+        String[] array = numbers.split(" ");
+        return String.format("%d %d",
+                Arrays.stream(array).mapToInt(Integer::parseInt).max().getAsInt(),
+                Arrays.stream(array).mapToInt(Integer::parseInt).min().getAsInt());
+    }
+}
